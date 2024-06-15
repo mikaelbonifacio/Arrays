3974f533cfc4d2f1d287b34e140dc708d5968134
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-14 23:29:01 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros =  new int[5];
+        numeros[0]  = 1;
+
+        System.out.printf("Hello and welcome!");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
